fcd0f0025f26924fc8bc2b2e1b3ea6afbdeae44a
yonnick.io <yonnick00@gmail.com>
2018-07-20 21:54:58 +0100

    exercise folder

---
+++ exercises/exercise_methods.rb
@@ -0,0 +1,21 @@
+=begin 1
+def greeting(name)
+  "hello #{name}, how you doing?"
+end
+
+puts greeting("John")
+=end
+
+=begin 2
+def multiply(a,b)
+  a * b
+end
+
+puts multiply(2,7).to_i
+=end
+
+def scream(words)
+  words = words + "!!!!"
+end
+
+puts scream("Yippeee")
--- exercises/exercise_variables.rb
+++ exercises/exercise_variables.rb
@@ -1,4 +1,30 @@
+## name.rb
 ## puts "What is your name?"
 ## name = gets.chomp
 ## puts  "G'day " + name +"!"
 
+=begin age.rb
+puts "How old are you? "
+age = gets.chomp.to_i
+puts "In 10 years you will be #{age+10}" 
+puts "In 20 years you will be #{age+20}" 
+puts "In 30 years you will be #{age+30}" 
+puts "In 40 years you will be #{age+40}" 
+=end
+
+=begin name.rb 2
+puts "What is your name?"
+name = gets.chomp
+
+10.times do
+  puts name
+end
+=end
+
+=begin name.rb
+puts "What is your first name?"
+f_name = gets.chomp
+puts "What is your last name?"
+l_name = gets.chomp
+puts "G'day #{f_name} #{l_name}"
+=end
